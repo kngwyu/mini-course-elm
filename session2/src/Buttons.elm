cda33553d9c756f1893b31098b9bc08f77f98011
kngwyu <yuji.kngw.80s.revive@gmail.com>
2021-07-14 10:04:04 +0900

    Did some exercises

--- session2/src/Buttons.elm
+++ session2/src/Buttons.elm
@@ -66,6 +66,11 @@ update msg model =
 -- VIEW
 
 
+scarlet : Color
+scarlet =
+    El.fromRgb255 { red = 190, green = 1, blue = 25, alpha = 1 }
+
+
 darkBlue : Color
 darkBlue =
     El.fromRgb255 { red = 8, green = 78, blue = 126, alpha = 1 }
@@ -76,42 +81,58 @@ white =
     El.fromRgb255 { red = 255, green = 255, blue = 255, alpha = 1 }
 
 
-fancyButton : String -> Msg -> Element Msg
-fancyButton text msg =
+fancyButton : String -> Msg -> Int -> Element Msg
+fancyButton text msg width =
     Input.button
         [ Background.color darkBlue
         , Border.rounded 10
         , Font.color white
-        , El.width (El.px 40)
+        , El.width (El.px width)
         , El.height (El.px 40)
+        , El.centerX
         ]
         { label = El.text text |> El.el [ El.centerX, El.centerY ]
         , onPress = Just msg
         }
 
 
-fancyText : String -> Element msg
-fancyText text =
+smallFancyButton : String -> Msg -> Element Msg
+smallFancyButton text msg =
+    fancyButton text msg 40
+
+
+fancyText : String -> Color -> Element msg
+fancyText text textColor =
     El.text text
         |> El.el [ El.centerX, El.centerY, El.moveDown 1.0 ]
         |> El.el
             [ El.width (El.px 40)
             , El.height (El.px 40)
-            , Font.color darkBlue
+            , Font.color textColor
             , Font.bold
             , Font.family [ Font.sansSerif, Font.typeface "Helvetica" ]
             , Border.width 2
             , Border.color darkBlue
             , Border.rounded 10
+            , El.centerX
             ]
+
+
+selectColor : Int -> Color
+selectColor count =
+    if count < 0 then
+        scarlet
+
+    else
+        darkBlue
 
 
 view : Model -> Html Msg
 view model =
-    [ fancyButton "-" Decrement
-    , fancyText (String.fromInt model)
-    , fancyButton "+" Increment
-    , Input.button [] { label = El.text "Reset", onPress = Just Reset }
+    [ smallFancyButton "-" Decrement
+    , fancyText (String.fromInt model) (selectColor model)
+    , smallFancyButton "+" Increment
+    , fancyButton "Reset" Reset 100
     ]
         |> El.column [ El.centerX, El.centerY, El.padding 20, Font.size 20, El.spacing 5 ]
         |> El.layout []
--- session2/src/OIST.elm
+++ session2/src/OIST.elm
@@ -12,6 +12,7 @@ module OIST exposing (..)
 
 import Element as El exposing (Color, Element)
 import Element.Background as Background
+import Element.Border as Border
 import Element.Font as Font
 import Html exposing (Html)
 
@@ -35,9 +36,24 @@ grey =
     El.fromRgb255 { red = 153, green = 153, blue = 153, alpha = 1 }
 
 
+lightGrey : Color
+lightGrey =
+    El.fromRgb255 { red = 221, green = 221, blue = 221, alpha = 1 }
+
+
 oistRed : Color
 oistRed =
     El.fromRgb255 { red = 183, green = 37, blue = 37, alpha = 1 }
+
+
+urlRed : Color
+urlRed =
+    El.fromRgb255 { red = 199, green = 0, blue = 25, alpha = 1 }
+
+
+textColor : Color
+textColor =
+    El.fromRgb255 { red = 68, green = 68, blue = 68, alpha = 1 }
 
 
 
@@ -102,13 +118,24 @@ viewMenuItem text =
 
 body : Element msg
 body =
-    El.column [ El.spacing 10, El.width El.fill, El.alignTop ]
+    El.column [ El.spacing 10, El.fill |> El.maximum 800 |> El.width, El.alignTop ]
         [ El.text "Graduate School" |> El.el [ Font.size 20 ]
         , El.image [ El.width El.fill ]
             { src = "https://groups.oist.jp/sites/default/files/imce/u100437/D75_1184.jpg"
             , description = "OIST Building"
             }
         , welcomeMessage
+        , El.row [ El.spacing 30, El.padding 10 ]
+            [ subBody "Prospective Students"
+                "https://groups.oist.jp/sites/default/files/imce/u100437/For%20Prospectives.jpg"
+                (subBodyLink "PhD program, admissions, research internships and more." "https://admissions.oist.jp/")
+            , subBody "Current Students"
+                "https://groups.oist.jp/sites/default/files/imce/u100437/For%20Students.jpg"
+                (subBodyLink "Microcourses, forms, scholarships, career paths and more." "https://groups.oist.jp/grad/current-students")
+            , subBody "Faculty and Stuff"
+                "https://groups.oist.jp/sites/default/files/imce/u100437/For%20Admin%20and%20Faculty.jpg"
+                (subBodyLink "Course schedules, microcourses, research interns and more." "https://groups.oist.jp/grad/oist-research-community")
+            ]
         ]
 
 
@@ -118,3 +145,41 @@ welcomeMessage =
         [ El.paragraph [] [ El.text "Welcome to the OIST Graduate School website." ]
         , El.paragraph [] [ El.text "The mission of the Graduate School is to provide first-class postgraduate education to the highest international standard. We recognize every student as a unique individual in need of a personalized program that we provide through courses, independent study and extra-curricular activities." ]
         ]
+
+
+subBodyText : String -> Element msg
+subBodyText text =
+    El.text text
+        |> El.el [ El.paddingXY 10 5, El.centerX, El.centerY, El.moveDown 1.0 ]
+        |> El.el
+            [ Background.color lightGrey
+            , Font.color textColor
+            , Font.family [ Font.sansSerif, Font.typeface "Helvetica" ]
+            , El.centerX
+            ]
+
+
+subBody : String -> String -> Element msg -> Element msg
+subBody name imageLink link =
+    El.column [ El.spacing 10, El.width El.fill, El.alignTop ]
+        [ subBodyText name
+        , El.image [ El.width El.fill ]
+            { src = imageLink
+            , description = name
+            }
+        , El.paragraph [] [ link ]
+        ]
+
+
+subBodyLink : String -> String -> Element msg
+subBodyLink text url =
+    El.newTabLink [ El.width El.fill, El.alignTop, El.mouseOver [ Font.size 24 ] ]
+        { url = url
+        , label = El.text text
+        }
+        |> El.el
+            [ Font.color urlRed
+            , Font.size 16
+            , Font.family [ Font.sansSerif, Font.typeface "Helvetica" ]
+            , El.centerX
+            ]
